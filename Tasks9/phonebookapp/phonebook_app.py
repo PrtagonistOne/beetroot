c58ef9ccaf66756982648d6c513296276e110c82
PrtagonistOne <playerdna4@gmail.com>
2021-08-11 19:17:46 +0300

    Added some logic

--- Tasks9/phonebookapp/phonebook_app.py
+++ Tasks9/phonebookapp/phonebook_app.py
@@ -1,5 +1,23 @@
-import json
 import phonebook_func
+
 
 user_session_flag = True
 while user_session_flag:
+    user_choice = int(input('\t****  THIS IS A PHONEBOOK APP ****\nChoose from the list below.\n1. Add a new contact\n'
+                            '2. Search by first name\n3. Search by last name\n4. Search by full name\n5.Search by phone'
+                            ' number\n6. Search by city or state\n7. Delete a record for a given telephone number\n8. '
+                            'Update a record for a given telephone number\n9. Exit\nInput number from 1 to 9: '))
+    if user_choice == 1:
+        phonebook_func.add_contact()
+    elif user_choice == 2:
+        phonebook_func.search(user_choice)
+    elif user_choice == 3:
+        phonebook_func.search(user_choice)
+    elif user_choice == 4:
+        phonebook_func.search(user_choice)
+    elif user_choice == 5:
+        phonebook_func.search(user_choice)
+    elif user_choice == 6:
+        phonebook_func.search(user_choice)
+    elif user_choice == 7:
+        print()
---
+++ Tasks9/phonebookapp/phonebook_func.py
@@ -0,0 +1,60 @@
+import json
+
+
+def search_read():
+    with open('phonebook_data.json', 'r') as f:
+        data = json.load(f)
+    return data
+
+
+def del_contact():
+    print()
+
+
+def search(option):
+    data = search_read()
+    if option == 2:
+        first_name = input('Input first name: ').strip().title()
+        for index, value in enumerate(data['contacts']):
+            if value['First name'] == first_name:
+                print(value)
+    elif option == 3:
+        last_name = input('Input last name: ').strip().title()
+        for index, value in enumerate(data['contacts']):
+            if value['Last name'] == last_name:
+                print(value)
+    elif option == 4:
+        full_name = input('Input full name: ').strip().title()
+        for index, value in enumerate(data['contacts']):
+            if value['Full name'] == full_name:
+                print(value)
+    elif option == 5:
+        phone_num = input('Input phone number: ').strip()
+        for index, value in enumerate(data['contacts']):
+            if value['Phone number'] == phone_num:
+                print(value)
+    elif option == 5:
+        city_state = input('Input city or state: ').strip()
+        for index, value in enumerate(data['contacts']):
+            if value['City or State'] == city_state:
+                print(value)
+
+
+def add_contact():
+    print('\tNew contact MUST have at least a PHONE NUMBER and it has to be 10 characters long to be added!')
+    data = {'Phone number': None, 'First name': None, 'Last name': None, 'City or State': None}
+    for key, value in data.items():
+        data[key] = input(f'{key}: ').title()
+    if (data['First name'] is not None) and (data['Last name'] is not None):
+        data['Full name'] = data['First name'].title() + ' ' + data['Last name'].title()
+    if data['Phone number'] is None or len(data['Phone number']) is not 10:
+        data.clear()
+        print('Contact was not added, try again')
+    else:
+        book_data = search_read()
+        book_data_list = book_data['contacts']
+        book_data_list.append(data)
+        book_data['contacts'] = book_data_list
+        with open('phonebook_data.json', 'w') as f:
+            json.dump(book_data, f)
+        print('New contact was added successfully!')
