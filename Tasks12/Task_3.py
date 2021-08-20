8d12d41fad41b456eb4d9f4c606f8afd754c8dbf
PrtagonistOne <playerdna4@gmail.com>
2021-08-20 23:29:36 +0300

    Task 3 is ready?

--- Tasks12/Task_3.py
+++ Tasks12/Task_3.py
@@ -1,7 +1,20 @@
-class Fraction(int):
+class Fraction(float):
     def __init__(self, math_expression):
         self.math_expression = math_expression
 
+    def __str__(self):
+        return str(self.math_expression)
+
+    def __repr__(self):
+        return self.__str__()
+
 
 x = Fraction(1/2)
-print(x.math_expression)
+y = Fraction(1/4)
+
+print(x + y == Fraction(3/4))
+
+x = Fraction(2 + 4)
+y = Fraction(3 + 3)
+
+print(x and y == Fraction(10 - 4))
