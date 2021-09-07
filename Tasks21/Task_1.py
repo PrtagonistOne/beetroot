256d8e0b8c00ec9f36e8ced907a18616e5a3b78b
PrtagonistOne <playerdna4@gmail.com>
2021-09-07 21:49:50 +0300

    Tasks20, Task 1 done

---
+++ Tasks21/Task_1.py
@@ -0,0 +1,53 @@
+class Boss:
+
+    def __init__(self, id_: int, name: str, company: str):
+        self.id = id_
+        self.name = name
+        self.company = company
+        self.workers = []
+
+    @property
+    def get_workers(self) -> list:
+        return self.workers
+
+    def add_worker(self, worker) -> None:
+        self.workers.append(worker)
+        print(f'A new worker "{worker}" was added!')
+
+    def __str__(self) -> str:
+        return f'id: {self.id}, name: {self.name}'
+
+    def __repr__(self) -> str:
+        return Boss.__str__(self)
+
+
+class Worker:
+    def __init__(self, id_: int, name: str, company: str, boss: Boss):
+        self.id = id_
+        self.name = name
+        self.company = company
+        self.boss = boss
+
+    @property
+    def get_boss(self) -> Boss:
+        return self.boss
+
+    @get_boss.setter
+    def get_boss(self, boss: Boss) -> None:
+        if isinstance(boss, Boss):
+            self.boss = boss
+            boss.workers.append(self)
+            print('boss setter invoked')
+        else:
+            raise ValueError('You can pass only Boss objects')
+
+    @get_boss.deleter
+    def get_boss(self) -> None:
+        print('Worker have moved to a different company\'s boss')
+        del self.boss
+
+    def __str__(self) -> str:
+        return f'id: {self.id}, name: {self.name}'
+
+    def __repr__(self)-> str:
+        return Worker.__str__(self)
