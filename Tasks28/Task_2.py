bc9bdb9c0b8e681e702afc203e50a2bd96b2124b
PrtagonistOne <playerdna4@gmail.com>
2021-09-27 19:19:33 +0300

    Tasks28 are done

---
+++ Tasks28/Task_2.py
@@ -0,0 +1,71 @@
+from functools import wraps
+from time import time
+
+
+def timeit(f):
+    @wraps(f)
+    def wrap(*args, **kw):
+        ts = time()
+        result = f(*args, **kw)
+        te = time()
+        print('func:%r args:[%r, %r] took: %2.4f sec' % (f.__name__, args, kw, te - ts))
+        return result
+
+    return wrap
+
+
+@timeit
+def FibonacciSearch(lys, val):
+    fibM_minus_2 = 0
+    fibM_minus_1 = 1
+    fibM = fibM_minus_1 + fibM_minus_2
+    while fibM < len(lys):
+        fibM_minus_2 = fibM_minus_1
+        fibM_minus_1 = fibM
+        fibM = fibM_minus_1 + fibM_minus_2
+    index = -1
+    while fibM > 1:
+        i = min(index + fibM_minus_2, (len(lys) - 1))
+        if lys[i] < val:
+            fibM = fibM_minus_1
+            fibM_minus_1 = fibM_minus_2
+            fibM_minus_2 = fibM - fibM_minus_1
+            index = i
+        elif lys[i] > val:
+            fibM = fibM_minus_2
+            fibM_minus_1 = fibM_minus_1 - fibM_minus_2
+            fibM_minus_2 = fibM - fibM_minus_1
+        else:
+            return i
+    if fibM_minus_1 and index < (len(lys) - 1) and lys[index + 1] == val:
+        return index + 1
+    return -1
+
+
+@timeit
+def binary_search(arr, x):
+    low = 0
+    high = len(arr) - 1
+    mid = 0
+
+    while low <= high:
+
+        mid = (high + low) // 2
+
+        if arr[mid] < x:
+            low = mid + 1
+
+        elif arr[mid] > x:
+            high = mid - 1
+
+        else:
+            return mid
+
+    return -1
+
+
+binary_search([i for i in range(100)], 100)
+FibonacciSearch([i for i in range(100)], 100)
+l = []
+l.__contains__()
+l.__len__()
---
+++ Tasks28/test.py
@@ -0,0 +1,6 @@
+book = {}
+
+book['me'] = 0
+book['me'] = 1
+
+print(book)
