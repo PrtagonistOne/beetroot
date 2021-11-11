9a855f395ac0fa49162208fe3d2cb837c489276a
PrtagonistOne <playerdna4@gmail.com>
2021-11-11 21:47:57 +0200

    Chapter 3 "Web forms" flask ready

--- Module_5/Tasks42/microblog/app/__init__.py
+++ Module_5/Tasks42/microblog/app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+import os
+
+
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 from . import routes
