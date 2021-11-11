44495a82097f679fb55d48383e1b23d22c35e962
PrtagonistOne <playerdna4@gmail.com>
2021-11-11 18:27:25 +0200

    A little rename and flask tutorial start

--- Module 5/Tasks41/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .departments import Departments
-from .countries import Countries
-from .emloyees import Employees
-from .jobs import Jobs
-from .locations import Locations
--- Module 5/Tasks41/run_app.py
+++
@@ -1,4 +0,0 @@
-from app import run_app
-
-if __name__ == '__main__':
-    run_app()
