9f341f787750bc4b98a9adf2072b380f159e8c0e
PrtagonistOne <playerdna4@gmail.com>
2021-11-11 22:48:40 +0200

    Chapter 4 "Database" flask ready

--- Module_5/Tasks42/microblog/app/__init__.py
+++ Module_5/Tasks42/microblog/app/__init__.py
@@ -1,11 +1,21 @@
 from flask import Flask
 import os
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # ...
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+                              'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 from . import routes
