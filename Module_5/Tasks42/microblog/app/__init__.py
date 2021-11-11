f7c789926f9e31afb999c58e37b481a104e242a4
PrtagonistOne <playerdna4@gmail.com>
2021-11-11 23:33:28 +0200

    Chapter 5 "User Logins" flask ready

--- Module_5/Tasks42/microblog/app/__init__.py
+++ Module_5/Tasks42/microblog/app/__init__.py
@@ -2,18 +2,21 @@ from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    # ...
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 app = Flask(__name__)
+login = LoginManager(app)
+login.login_view = 'login'
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
