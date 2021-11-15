d5065d821cae8ec208fc23b81c3df4728291d81e
PrtagonistOne <playerdna4@gmail.com>
2021-11-15 21:05:03 +0200

    DRF tutorial part 4 ready

--- Module_5/Tasks44/tutorial/snippets/urls.py
+++ Module_5/Tasks44/tutorial/snippets/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path, include
 from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>', views.snippet_detail),
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    path('user/<int:pk>/', views.UserDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
+
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls'))
 ]
