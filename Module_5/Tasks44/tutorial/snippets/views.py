5a2d27f53f90b1e1e5cd95499705145f4cccdc18
PrtagonistOne <playerdna4@gmail.com>
2021-11-16 14:05:24 +0200

    DRF tutorial part 5, Hyperlinks and Pagination  ready

--- Module_5/Tasks44/tutorial/snippets/views.py
+++ Module_5/Tasks44/tutorial/snippets/views.py
@@ -3,6 +3,27 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 from snippets.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
+from rest_framework import renderers
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = [renderers.StaticHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlight)
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format),
+    })
 
 
 class UserList(generics.ListAPIView):
